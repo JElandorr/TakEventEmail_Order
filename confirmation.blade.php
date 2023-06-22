f2cb4224018393c05e5e5bbc4d886ac97ab59f1d
JElandorr <102660718+JElandorr@users.noreply.github.com>
2023-06-22 15:32:07 +0300

    Update

--- confirmation.blade.php
+++
@@ -1,9 +0,0 @@
-<x-mail::message>
-    <x-mail::table>
-        | Item      | Quantity | Price |
-        | --------- | -------: | ----: |
-        @foreach ($order->items as $item)
-            | {{ $item->ticket->ticket_type->name }} | {{ $item->quantity }} | {{ $order->currency->prefix }}{{ $item->price }}{{ $order->currency->suffix }} |
-        @endforeach
-    </x-mail::table>
-</x-mail::message>
